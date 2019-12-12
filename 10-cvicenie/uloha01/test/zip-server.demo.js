4c9e943cd75b33b45b40430fdd2cd365b0be6304
Denisa Mensatorisová <32281312+DeMensa123@users.noreply.github.com>
2019-12-12 21:21:46 +0100

    Cviko10-oprava

--- 10-cvicenie/uloha01/test/zip-server.demo.js
+++ 10-cvicenie/uloha01/test/zip-server.demo.js
@@ -1,7 +1,7 @@
 const { spawn } = require('child_process');
 const path = require('path')
 
-const server = spawn('node', [path.join(__dirname, '..','src', 'zip-server.js')])
+const server = spawn('node', [path.join(__dirname, '..','src', 'uloha01','zip-server.js')])
 server.stdout.on('data', (data) => {
     console.log('STDOUT/server:', data.toString())
 });
@@ -12,8 +12,8 @@ server.stderr.on('data', (data) => {
 
 const client = spawn('node', [
     path.join(__dirname, '..','src', 'zip-client.js'),
-    path.join(__dirname, '..','src','client-data', 'cvicenie10.pptx'), 
-    `--output-file=${path.join(__dirname, '..','src','client-data', 'pptx_archive.zip')}`
+    path.join(__dirname, '..','src', 'test_file.txt'), 
+    `--output-file=${path.join(__dirname, '..','src', 'pptx_archive.zip')}`
 ])
 client.stdout.on('data', (data) => {
     console.log('STDOUT/client:', data.toString())
